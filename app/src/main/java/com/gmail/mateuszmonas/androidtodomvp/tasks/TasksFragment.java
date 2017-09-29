18b24a41efd4a3c7d1ebb55c4f5195774931c80d
mirek <mateuszmonas@gmail.com>
2017-09-29 18:16:43 +0200

    commit

--- app/src/main/java/com/gmail/mateuszmonas/androidtodomvp/tasks/TasksFragment.java
+++ app/src/main/java/com/gmail/mateuszmonas/androidtodomvp/tasks/TasksFragment.java
@@ -1,12 +1,26 @@
 package com.gmail.mateuszmonas.androidtodomvp.tasks;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.gmail.mateuszmonas.androidtodomvp.R;
 
 public class TasksFragment extends Fragment implements TasksContract.View {
 
     private TasksContract.Presenter presenter;
 
     public TasksFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_tasks, container, false);
+        return view;
     }
 
     @Override
