571973309475f94d5e41f307c67ed4a51a0a437d
mirek <mateuszmonas@gmail.com>
2017-09-29 19:51:58 +0200

    commit

--- app/src/main/java/com/gmail/mateuszmonas/androidtodomvp/tasks/TasksComponent.java
+++ app/src/main/java/com/gmail/mateuszmonas/androidtodomvp/tasks/TasksComponent.java
@@ -1,11 +1,12 @@
 package com.gmail.mateuszmonas.androidtodomvp.tasks;
 
+import com.gmail.mateuszmonas.androidtodomvp.data.DataRepositoryComponent;
 import com.gmail.mateuszmonas.androidtodomvp.utils.FragmentScope;
 
 import dagger.Component;
 
 @FragmentScope
-@Component(modules = TasksPresenterModule.class)
+@Component(dependencies = DataRepositoryComponent.class, modules = TasksPresenterModule.class)
 public interface TasksComponent {
 
     void inject(TasksActivity activity);
