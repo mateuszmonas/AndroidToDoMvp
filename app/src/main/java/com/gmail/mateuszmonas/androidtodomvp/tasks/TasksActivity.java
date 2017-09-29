85a8d11694416f2dad9b5b8333611b443c94ab32
mirek <mateuszmonas@gmail.com>
2017-09-29 17:35:15 +0200

    commit

--- app/src/main/java/com/gmail/mateuszmonas/androidtodomvp/tasks/TasksActivity.java
+++ app/src/main/java/com/gmail/mateuszmonas/androidtodomvp/tasks/TasksActivity.java
@@ -3,10 +3,20 @@ package com.gmail.mateuszmonas.androidtodomvp.tasks;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.gmail.mateuszmonas.androidtodomvp.R;
+import com.gmail.mateuszmonas.androidtodomvp.utils.ActivityUtils;
+
 public class TasksActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tasks);
+
+        TasksFragment tasksFragment = (TasksFragment) getSupportFragmentManager().findFragmentById(R.id.contentFrame);
+        if (tasksFragment == null){
+            tasksFragment = TasksFragment.newInstance();
+            ActivityUtils.addFragmentToActivity(getSupportFragmentManager(), tasksFragment, R.id.contentFrame);
+        }
     }
 }
--- app/src/main/java/com/gmail/mateuszmonas/androidtodomvp/tasks/TasksFragment.java
+++ app/src/main/java/com/gmail/mateuszmonas/androidtodomvp/tasks/TasksFragment.java
@@ -1,12 +1,21 @@
 package com.gmail.mateuszmonas.androidtodomvp.tasks;
 
+import android.support.v4.app.Fragment;
 
-public class TasksFragment implements TasksContract.View {
+public class TasksFragment extends Fragment implements TasksContract.View {
 
     private TasksContract.Presenter presenter;
+
+    public TasksFragment() {
+    }
 
     @Override
     public void setPresenter(TasksContract.Presenter presenter) {
         this.presenter=presenter;
     }
+
+    public static TasksFragment newInstance(){
+        return new TasksFragment();
+    }
+
 }
