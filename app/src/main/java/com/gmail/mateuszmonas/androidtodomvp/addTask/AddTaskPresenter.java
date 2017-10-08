3e02d15fffee371f24a6fe321359051028a8fcf9
mirek <mateuszmonas@gmail.com>
2017-10-08 13:24:04 +0200

    commit

--- app/src/main/java/com/gmail/mateuszmonas/androidtodomvp/addTask/AddTaskPresenter.java
+++ app/src/main/java/com/gmail/mateuszmonas/androidtodomvp/addTask/AddTaskPresenter.java
@@ -1,7 +1,25 @@
 package com.gmail.mateuszmonas.androidtodomvp.addTask;
 
 
+import com.gmail.mateuszmonas.androidtodomvp.data.DataRepository;
+
+import javax.inject.Inject;
+
 public class AddTaskPresenter implements AddTaskContract.Presenter {
+
+    private final AddTaskContract.View view;
+    private final DataRepository repository;
+
+    @Inject
+    public AddTaskPresenter(AddTaskContract.View view, DataRepository repository) {
+        this.view = view;
+        this.repository = repository;
+    }
+
+    @Inject
+    void setupListeners() {
+        view.setPresenter(this);
+    }
 
     @Override
     public void start() {
